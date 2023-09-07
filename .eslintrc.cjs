32ce21e97aabf273ec8835a54f506ec772148a5c
kjing <jk021500@163.com>
2023-09-07 18:11:50 +0800

    feat: add some devDependencies
    1. eslint-config-standard etc.
    2. husky etc.
    3. normalize.css
    4. unplugin-auto-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,17 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    root: true,
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/eslint-config-standard',
+        '@vue/eslint-config-typescript'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        // 首行缩进 4 个字符
+        indent: ['error', 4]
+    }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import TheWelcome from '../components/TheWelcome.vue'
+// no need to import ref
+const a = ref(0)
+console.log(a.value)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <TheWelcome/>
+    </main>
+    <div>
+    </div>
 </template>
