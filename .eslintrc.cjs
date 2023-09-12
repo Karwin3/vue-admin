310d5771732c153fb0f6a009affb0a6d1d36caa9
kjing <jk021500@163.com>
2023-09-12 17:04:28 +0800

    feat: add dependencies and delete template file 1. element ui plus 2. axios

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,16 @@ module.exports = {
     extends: [
         'plugin:vue/vue3-essential',
         '@vue/eslint-config-standard',
-        '@vue/eslint-config-typescript'
+        '@vue/eslint-config-typescript',
+        './.eslintrc-auto-import.json'
     ],
     parserOptions: {
         ecmaVersion: 'latest'
     },
     rules: {
         // 首行缩进 4 个字符
-        indent: ['error', 4]
+        indent: ['error', 4],
+        // 关闭组件命名规则
+        'vue/multi-word-component-names': 'off',
     }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,55 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        AutoImport({
-            // 支持依赖选项
-            imports: ['vue', 'vue-router', 'pinia']
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [
+            vue(),
+            AutoImport({
+                // 支持依赖选项
+                imports: ['vue', 'vue-router', 'pinia'],
+                eslintrc: {
+                    enabled: true
+                },
+                resolvers: [
+                    ElementPlusResolver(),
+                    IconsResolver({
+                        prefix: 'Icon'
+                    })]
+            }),
+            Components({
+                resolvers: [
+                    ElementPlusResolver(),
+                    IconsResolver({
+                        enabledCollections: ['ep']
+                    })
+                ]
+            }),
+            Icons({
+                autoInstall: true
+            })
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        server: {
+            proxy: {
+                [env.VITE_APP_BASE_PREFIX + '/front']: {
+                    target: 'http://39.97.218.60/',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(env.VITE_APP_BASE_PREFIX, '')
+                }
+            }
         }
     }
 })
